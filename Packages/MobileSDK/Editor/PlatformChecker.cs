48ab3ed9a6c637355c988f9da4ce174b5bdb8ca9
Martin Lazarov <lazarovmartin3@gmail.com>
2023-10-13 13:23:22 +0300

    init commit 2

---
+++ Packages/MobileSDK/Editor/PlatformChecker.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlatformChecker : MonoBehaviour
+{
+        ///Check if the program run on Android or iOS
+        /// ***********************
+
+        private GameObject button;
+        private Sprite toast;
+        private Sprite snackbar;
+
+
+        private void Start()
+        {
+            //btn.onClick.AddListener(ShowButtonImage);
+            Init();
+        }
+
+
+        private void ShowButtonImage()
+        {
+#if UNITY_IOS
+            button.GetComponent<Button>().image.sprite = snackbar;
+#elif UNITY_ANDROID
+            button.GetComponent<Image>().sprite = toast;
+#endif
+            //WeatherApp.instance.ShowTemp();
+        }
+
+        private void Init()
+        {
+            GameObject canvas = new GameObject();
+            canvas.transform.parent = transform;
+            canvas.name = "Canvas";
+            canvas.AddComponent<Canvas>();
+            canvas.AddComponent<CanvasScaler>();
+            canvas.AddComponent<GraphicRaycaster>();
+
+            canvas.GetComponent<Canvas>().renderMode = RenderMode.ScreenSpaceOverlay;
+
+            button = new GameObject();
+            button.name = "Click me!";
+            button.AddComponent<Button>();
+            button.AddComponent<RectTransform>();
+            button.AddComponent<Image>();
+            button.transform.SetParent(canvas.transform, false);
+            button.layer = 5;
+            button.GetComponent<RectTransform>().sizeDelta = new Vector2(300, 300);
+            button.GetComponent<RectTransform>().anchoredPosition.Set(0, 0);
+            button.GetComponent<Button>().onClick.AddListener(ShowButtonImage);
+
+            toast = Resources.Load<Sprite>("Sprites/toast");
+            snackbar = Resources.Load<Sprite>("Sprites/Snackbar");
+        }
+    }
